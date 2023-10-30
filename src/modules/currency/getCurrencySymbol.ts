8eb23ae3d69237525226c2d6cf50c636bbc3f1d9
RgnDunes <divyansh.singh@razorpay.com>
2023-10-30 19:11:53 +0530

    minor refactor

--- src/modules/currency/getCurrencySymbol.ts
+++ src/modules/currency/getCurrencySymbol.ts
@@ -1,9 +1,8 @@
 import { CURRENCY_DATA } from "./data/currencies";
 
-const  getCurrencySymbol = (currencyCode: string): string => {
-  if(currencyCode in CURRENCY_DATA)
-  return CURRENCY_DATA[currencyCode]?.symbol;
+const getCurrencySymbol = (currencyCode: string): string => {
+  if (currencyCode in CURRENCY_DATA) return CURRENCY_DATA[currencyCode]?.symbol;
   else throw new Error("Invalid currencyCode!");
-}
+};
 
 export default getCurrencySymbol;
