1b435cb16e77872e7315d0fcdbd9c7d9cc475cc1
RgnDunes <divyansh.singh@razorpay.com>
2023-10-30 19:11:10 +0530

    Add exception in getCurrencySymbol

--- src/modules/currency/getCurrencySymbol.ts
+++ src/modules/currency/getCurrencySymbol.ts
@@ -1,8 +1,9 @@
 import { CURRENCY_DATA } from "./data/currencies";
 
 const  getCurrencySymbol = (currencyCode: string): string | undefined => {
-  const currencyData = CURRENCY_DATA;
-  return currencyData[currencyCode]?.symbol;
+  if(currencyCode in CURRENCY_DATA)
+  return CURRENCY_DATA[currencyCode]?.symbol;
+  else throw new Error("Invalid currencyCode!");
 }
 
 export default getCurrencySymbol;
