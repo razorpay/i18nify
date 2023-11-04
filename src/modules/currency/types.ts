93459ae947a0767b4d80cabd11c19bbfa9858c42
RgnDunes <divyansh.singh@razorpay.com>
2023-11-05 01:31:18 +0530

    add formatAmountByParts

--- src/modules/currency/types.ts
+++ src/modules/currency/types.ts
@@ -8,3 +8,11 @@ export interface Currency {
 export interface GetCurrencyListOutput {
   [currencyCode: keyof typeof CURRENCIES]: Currency;
 }
+
+export interface ByParts {
+  currencySymbol: string;
+  integerValue: string;
+  decimalValue: string;
+  separator: string;
+  symbolAtFirst: boolean;
+}
