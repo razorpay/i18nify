8a79c246f02dadbfba333c19414bdee5618893dd
Tarun Khanna <tarun.khanna@razorpay.com>
2023-11-08 13:13:59 +0530

    [feat]: danger added for bundle size report and good practices (#15)

---
+++ dangerfile.js
@@ -0,0 +1,93 @@
+// refer file: https://gist.github.com/kkemple/998aad9f2b25520c916b00891abb6543
+import { danger, fail, warn, markdown } from 'danger';
+
+const { generateTableReport } = require('./scripts/bundle-size-report');
+
+const modifiedFiles = danger.git.modified_files;
+const newFiles = danger.git.created_files;
+const pr = danger.github.pr;
+
+/**
+ * Rule: Small pr is suggested.
+ * Reason: Pr is not supposed to be very large so it is suggested to keep the pr small.
+ *         So it would be easy for reviewer to review and reduce chances of missing bugs.
+ *         Warn when there is a big PR
+ */
+const bigPRThreshold = 600;
+if (pr.additions > bigPRThreshold) {
+  warn(
+    `Your PR has over ${bigPRThreshold} lines of code additions :scream: . Try to breakup into separate PRs :+1:`,
+  );
+}
+
+/**
+ * Rule: 1-2 reviewer is required.
+ * Reason: No reviewer tends to leave a PR in a state where nobody is
+ *         responsible. Similarly, more than 2 reviewer doesn't clearly state
+ *         who is responsible for the review. 2 reviewrs can speed up review as well
+ *         as suppose 1 suppose to be primary reviewer and other is secondary reviewer
+ */
+// Get total reviewers assigned by the user
+const requestedReviewers = danger.github.requested_reviewers;
+const requestedReviewersCount =
+  requestedReviewers.users.length + requestedReviewers.teams.length;
+// Get total distinct reviewers who have provided the review
+const reviews = danger.github.reviews;
+let reviewers = [];
+for (let i = 0; i < reviews.length; i++) {
+  if (reviews[i].user.type === 'User') {
+    if (!reviewers.includes(reviews[i].id)) {
+      reviewers.push(reviews[i].id);
+    }
+  }
+}
+const reviewersCount = reviewers.length;
+// either PR should already be reviewed or reviewers must be added
+if (requestedReviewersCount === 0 && reviewersCount === 0) {
+  fail(`🕵 Whoops, I don't see any reviewers. Remember to add one.`);
+} else if (requestedReviewersCount > 2) {
+  warn(
+    `It's great to have ${requestedReviewersCount} reviewers. Remember though ` +
+      `that more than 2 reviewer may lead to uncertainty as to who is responsible for the review.`,
+  );
+}
+
+/**
+ * Rule: File size should be less than threshold
+ * Reason: Aim is to move towards a more modularized code, hence try to break large files in separate functions/components.
+ */
+const fileSizeThreshold = 300;
+for (let file of newFiles) {
+  const fileUrl = danger.github.utils.fileLinks([file]);
+
+  danger.git.structuredDiffForFile(file).then((res) => {
+    if (res.chunks[0] && res.chunks[0].newLines > fileSizeThreshold) {
+      warn(
+        `this ${fileUrl} has over ${bigPRThreshold} lines of code additions :scream: . Try to breakup into separate functions/components :+1:`,
+      );
+    }
+  });
+}
+
+/**
+ * Rule: Warn if package.json is modified
+ * Reason: We do not want to increase our bundle size, hence every new lib added needs to be reviewed
+ *         by gatekeepers.
+ */
+const packageFile = modifiedFiles.find((file) => file.includes('package.json'));
+if (packageFile) {
+  const fileUrl = danger.github.utils.fileLinks([packageFile]);
+  warn(
+    `**${fileUrl}**: package.json is modified, do get this reviewed by gatekeepers ! 😇`,
+  );
+}
+
+const showBundleSizeReport = () => {
+  markdown(`
+## Bundle Size Report
+
+${generateTableReport()}
+`);
+};
+
+showBundleSizeReport();
---
+++ scripts/bundle-size-report.js
@@ -0,0 +1,142 @@
+const fs = require('fs');
+
+function safeChunkSize(size) {
+  const isNumber = (v) => typeof v === 'number';
+  const inKb = (v) => v / 1024;
+
+  return isNumber(size) ? inKb(size) : 0;
+}
+
+function round(v) {
+  return Math.round(v * 1e2) / 1e2;
+}
+
+function extractStatsForChunk(chunkName, baseStats, prStats) {
+  //parsed size
+  const baseParseSize = round(
+    safeChunkSize(baseStats[chunkName] && baseStats[chunkName].parsed),
+  );
+  const prParseSize = round(
+    safeChunkSize(prStats[chunkName] && prStats[chunkName].parsed),
+  );
+
+  return {
+    baseSize: { parsed: baseParseSize },
+    prSize: { parsed: prParseSize },
+    difference: {
+      parsed: round(prParseSize - baseParseSize),
+    },
+    differencePercent: {
+      parsed: round(((prParseSize - baseParseSize) * 100) / baseParseSize),
+    },
+  };
+}
+
+/**
+ * output
+ * {
+ *   abc.js: { base, pr, diff, diff% },
+ * }
+ */
+function calculateDifferences(baseStats, prStats) {
+  const initialChunks = Array.from(
+    new Set([...Object.keys(baseStats).concat(Object.keys(prStats))]),
+  ).sort();
+
+  const report = Object.fromEntries(
+    initialChunks.map((chunkName) => [
+      chunkName,
+      extractStatsForChunk(chunkName, baseStats, prStats),
+    ]),
+  );
+
+  return report;
+}
+
+function differenceToSymbol(v) {
+  // new chunk found
+  if (v === Infinity) {
+    return '🆕';
+  }
+
+  // chunk deleted
+  if (v === -100) {
+    return '🗑';
+  }
+
+  // pr chunk size increased
+  if (v > 0) {
+    return '📈';
+  }
+
+  // pr chunk size reduced
+  if (v < 0) {
+    return '👍';
+  }
+
+  return '🟢';
+}
+
+function traverseFileAndGenerateStats(dir) {
+  const filesToTrack = ['esm/index.js', 'cjs/index.js', 'umd/index.js'];
+
+  return filesToTrack.reduce((acc, curr) => {
+    const fullPath = `${dir}/${curr}`;
+    acc[curr] = { parsed: fs.lstatSync(fullPath).size };
+    return acc;
+  }, {});
+}
+
+function formatToTable(stat, statName) {
+  return [
+    `<details open>`,
+    `<summary><h3>${statName}</h3> <em>click to expand/collapse</em></summary>`,
+    '',
+    '| 🟢 No Change | 🗑 File Deleted | 🆕 New File | 📈 Size Increased | 👍 Size Reduced |',
+    '| --- | --- | --- | --- | --- |',
+    '',
+    '<table>',
+    '<tbody>',
+    '<tr> <td></td> <td></td> <th colspan="4">Parsed (kb)</th> </tr>',
+    '<tr>' +
+      ' <td>🚦</td>' +
+      ' <th>File Name</th>' +
+      ' <th>Base</th>' +
+      ' <th>PR</th>' +
+      ' <th>Diff</th>' +
+      ' <th>%</th>' +
+      '</tr>',
+    ...Object.entries(stat).map(([filename, fileStat]) => {
+      const diffColor = fileStat.difference.parsed <= 0 ? 'green' : 'red';
+      return `<tr>
+        <td>${differenceToSymbol(fileStat.differencePercent.parsed)}</td>
+        <td><strong><code>${filename} </code></strong></td>
+        <td><code>${fileStat.baseSize.parsed} </code></td>
+        <td><code> ${fileStat.prSize.parsed} </code></td>
+        <td> $\\textcolor{${diffColor}}{${fileStat.difference.parsed}}$ </td>
+        <td><code> ${
+          isFinite(fileStat.differencePercent.parsed)
+            ? fileStat.differencePercent.parsed
+            : '—'
+        } </code></td>
+      </tr>`;
+    }),
+    '</tbody>',
+    '</table>',
+    '</details/>',
+  ].join('\n');
+}
+
+function generateTableReport() {
+  const baseBuildPath = 'build/base';
+  const prBuildPath = 'build/pr';
+
+  const baseStats = traverseFileAndGenerateStats(baseBuildPath);
+  const prStats = traverseFileAndGenerateStats(prBuildPath);
+
+  const rawStats = calculateDifferences(baseStats, prStats);
+
+  return [formatToTable(rawStats, 'Files')].join('\n');
+}
+
+module.exports.generateTableReport = generateTableReport;
