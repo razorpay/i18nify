b1a0544a576898c722262ca635e2cd185b3d5564
RgnDunes <divyansh.singh@razorpay.com>
2023-11-21 02:43:06 +0530

    refactor regex

--- src/modules/phoneNumber/utils.ts
+++ src/modules/phoneNumber/utils.ts
@@ -16,7 +16,7 @@ export const detectCountryCodeFromDialCode = (
 
 export const removeNonNumericChars = (phoneNumber: string) => {
   // Regular expression to match all characters except numbers and + sign at the start
-  const regex = /^[^0-9+]+/g;
+  const regex = /[^0-9+]|(?!\A)\+/g;
   // Replace matched characters with an empty string
   const cleanedString = phoneNumber.replace(regex, '');
   return cleanedString;
