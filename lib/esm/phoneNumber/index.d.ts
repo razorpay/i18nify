39ad89ab7d0d18d0243404302962fc7dd306e443
RgnDunes <divyansh.singh@razorpay.com>
2023-11-21 02:01:53 +0530

    add build

--- lib/esm/phoneNumber/index.d.ts
+++ lib/esm/phoneNumber/index.d.ts
@@ -1,5 +1,13 @@
-declare const _default$1: (phoneNumber: string | number, countryCode?: string | number) => boolean;
+declare const _default$2: (phoneNumber: string | number, countryCode?: string | number) => boolean;
 
-declare const _default: (phoneNumber: string | number, countryCode?: string | number) => string;
+declare const _default$1: (phoneNumber: string | number, countryCode?: string | number) => string;
 
-export { _default as formatPhoneNumber, _default$1 as validatePhoneNumber };
+interface PhoneInfo {
+    countryCode: string;
+    dialCode: string;
+    formattedPhoneNumber: string;
+    formatTemplate: string;
+}
+declare const _default: (phoneNumber: string) => PhoneInfo;
+
+export { _default$1 as formatPhoneNumber, _default as parsePhoneNumber, _default$2 as validatePhoneNumber };
--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,4 +1,4 @@
-declare const _default$5: (amount: string | number, options?: {
+declare const _default$6: (amount: string | number, options?: {
     currency?: string | number;
     locale?: string;
     intlOptions?: Intl.NumberFormatOptions;
@@ -19,18 +19,26 @@ interface ByParts {
     symbolAtFirst: boolean;
 }
 
-declare const _default$4: () => GetCurrencyListOutput;
+declare const _default$5: () => GetCurrencyListOutput;
 
-declare const _default$3: (currencyCode: string | number) => string;
+declare const _default$4: (currencyCode: string | number) => string;
 
-declare const _default$2: (amount: string | number, options?: {
+declare const _default$3: (amount: string | number, options?: {
     currency?: string | number;
     locale?: string;
     intlOptions?: Intl.NumberFormatOptions;
 }) => ByParts;
 
-declare const _default$1: (phoneNumber: string | number, countryCode?: string | number) => boolean;
+declare const _default$2: (phoneNumber: string | number, countryCode?: string | number) => boolean;
 
-declare const _default: (phoneNumber: string | number, countryCode?: string | number) => string;
+declare const _default$1: (phoneNumber: string | number, countryCode?: string | number) => string;
 
-export { _default$5 as formatNumber, _default$2 as formatNumberByParts, _default as formatPhoneNumber, _default$4 as getCurrencyList, _default$3 as getCurrencySymbol, _default$1 as validatePhoneNumber };
+interface PhoneInfo {
+    countryCode: string;
+    dialCode: string;
+    formattedPhoneNumber: string;
+    formatTemplate: string;
+}
+declare const _default: (phoneNumber: string) => PhoneInfo;
+
+export { _default$6 as formatNumber, _default$3 as formatNumberByParts, _default$1 as formatPhoneNumber, _default$5 as getCurrencyList, _default$4 as getCurrencySymbol, _default as parsePhoneNumber, _default$2 as validatePhoneNumber };
