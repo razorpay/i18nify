4f7c6f7246161e3dea78f3d09d8748456fed4f28
RgnDunes <divyansh.singh@razorpay.com>
2023-11-27 11:45:27 +0530

    rename validatePhoneNumber to isValidPhoneNumber

--- src/modules/phoneNumber/index.ts
+++ src/modules/phoneNumber/index.ts
@@ -1,3 +1,3 @@
-export { default as validatePhoneNumber } from './validatePhoneNumber';
+export { default as isValidPhoneNumber } from './isValidPhoneNumber';
 export { default as formatPhoneNumber } from './formatPhoneNumber';
 export { default as parsePhoneNumber } from './parsePhoneNumber';
