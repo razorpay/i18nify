7768d8680735771a83c412b5d907470e6ef76a13
RgnDunes <divyansh.singh@razorpay.com>
2023-12-21 18:39:01 +0530

    add UT for entryfile

---
+++ src/__tests__/entryFile.test.ts
@@ -0,0 +1,26 @@
+// Mock the loadPolyfills function
+jest.mock('../modules/.internal/polyfills/index', () => ({
+  __esModule: true,
+  default: jest.fn(() => Promise.resolve()),
+}));
+
+const loadPolyfills = require('../modules/.internal/polyfills/index').default;
+
+describe('Entry File - index.js', () => {
+  beforeEach(() => {
+    jest.clearAllMocks(); // Reset mock state before each test
+  });
+
+  it('should load polyfills successfully', async () => {
+    const consoleInfoMock = jest.spyOn(console, 'info').mockImplementation();
+
+    // Trigger the function that triggers loading polyfills
+    await require('../index');
+
+    // Assertions
+    expect(loadPolyfills).toHaveBeenCalledTimes(1);
+    expect(consoleInfoMock).toHaveBeenCalledWith(
+      'Polyfills loaded successfully!',
+    );
+  });
+});
