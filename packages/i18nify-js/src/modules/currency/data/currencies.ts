cd90e175bd233787293c6d1e02a29f337b984de2
RgnDunes <divyansh.singh@razorpay.com>
2024-02-01 20:40:58 +0530

    update currencies onject

--- packages/i18nify-js/src/modules/currency/data/currencies.ts
+++ packages/i18nify-js/src/modules/currency/data/currencies.ts
@@ -224,182 +224,156 @@ export const CURRENCIES = {
   HTG: {
     symbol: 'G',
     name: 'Haitian Gourde',
-    minorUnits: 2,
     lowerUnitName: 'Centime',
     conversionRate: 100
   },
   HUF: {
     symbol: 'Ft',
     name: 'Hungarian Forint',
-    minorUnits: 2,
     lowerUnitName: 'Fillér',
     conversionRate: 100
   },
   IDR: {
     symbol: 'Rp',
     name: 'Indonesian Rupiah',
-    minorUnits: 2,
     lowerUnitName: 'Sen',
     conversionRate: 100
   },
   ILS: {
     symbol: '₪',
     name: 'Israeli New Shekel',
-    minorUnits: 2,
     lowerUnitName: 'Agora',
     conversionRate: 100
   },
   INR: {
     symbol: '₹',
     name: 'Indian Rupee',
-    minorUnits: 2,
     lowerUnitName: 'Paisa',
     conversionRate: 100
   },
   JMD: {
     symbol: 'J$',
     name: 'Jamaican Dollar',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   KES: {
     symbol: 'Ksh',
     name: 'Kenyan Shilling',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   KGS: {
     symbol: 'Лв',
     name: 'Kyrgystani Som',
-    minorUnits: 2,
     lowerUnitName: 'Tyiyn',
     conversionRate: 100
   },
   KHR: {
     symbol: '៛',
     name: 'Cambodian Riel',
-    minorUnits: 2,
     lowerUnitName: 'Sen',
     conversionRate: 100
   },
   KYD: {
     symbol: 'CI$',
     name: 'Cayman Islands Dollar',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   KZT: {
     symbol: '₸',
     name: 'Kazakhstani Tenge',
-    minorUnits: 2,
     lowerUnitName: 'Tiyn',
     conversionRate: 100
   },
   LAK: {
     symbol: '₭',
     name: 'Laotian Kip',
-    minorUnits: 2,
     lowerUnitName: 'Att',
     conversionRate: 100
   },
   LKR: {
     symbol: 'රු',
     name: 'Sri Lankan Rupee',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   LRD: {
     symbol: 'L$',
     name: 'Liberian Dollar',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   LSL: {
     symbol: 'LSL',
     name: 'Lesotho Loti',
-    minorUnits: 2,
     lowerUnitName: 'Sente',
     conversionRate: 100
   },
   MAD: {
     symbol: 'د.م.',
     name: 'Moroccan Dirham',
-    minorUnits: 2,
     lowerUnitName: 'Centime',
     conversionRate: 100
   },
   MDL: {
     symbol: 'MDL',
     name: 'Moldovan Leu',
-    minorUnits: 2,
     lowerUnitName: 'Ban',
     conversionRate: 100
   },
   MKD: {
     symbol: 'ден',
     name: 'Macedonian Denar',
-    minorUnits: 2,
     lowerUnitName: 'Deni',
     conversionRate: 100
   },
   MMK: {
     symbol: 'MMK',
     name: 'Myanmar Kyat',
-    minorUnits: 2,
     lowerUnitName: 'Pya',
     conversionRate: 100
   },
   MNT: {
     symbol: '₮',
     name: 'Mongolian Tugrik',
-    minorUnits: 2,
     lowerUnitName: 'Möngö',
     conversionRate: 100
   },
   MOP: {
     symbol: 'MOP$',
     name: 'Macanese Pataca',
-    minorUnits: 2,
     lowerUnitName: 'Avo',
     conversionRate: 100
   },
   MUR: {
     symbol: '₨',
     name: 'Mauritian Rupee',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   MVR: {
     symbol: 'Rf',
     name: 'Maldivian Rufiyaa',
-    minorUnits: 2,
     lowerUnitName: 'Laari',
     conversionRate: 100
   },
   MWK: {
     symbol: 'MK',
     name: 'Malawian Kwacha',
-    minorUnits: 2,
     lowerUnitName: 'Tambala',
     conversionRate: 100
   },
   MXN: {
     symbol: 'Mex$',
     name: 'Mexican Peso',
-    minorUnits: 2,
     lowerUnitName: 'Centavo',
     conversionRate: 100
   },
   MYR: {
     symbol: 'RM',
     name: 'Malaysian Ringgit',
-    minorUnits: 2,
     lowerUnitName: 'Sen',
     conversionRate: 100
   },
@@ -412,105 +386,90 @@ export const CURRENCIES = {
   NGN: {
     symbol: '₦',
     name: 'Nigerian Naira',
-    minorUnits: 2,
     lowerUnitName: 'Kobo',
     conversionRate: 100
   },
   NIO: {
     symbol: 'NIO',
     name: 'Nicaraguan Córdoba',
-    minorUnits: 2,
     lowerUnitName: 'Centavo',
     conversionRate: 100
   },
   NOK: {
     symbol: 'NOK',
     name: 'Norwegian Krone',
-    minorUnits: 2,
     lowerUnitName: 'Øre',
     conversionRate: 100
   },
   NPR: {
     symbol: 'रू',
     name: 'Nepalese Rupee',
-    minorUnits: 2,
     lowerUnitName: 'Paisa',
     conversionRate: 100
   },
   NZD: {
     symbol: 'NZ$',
     name: 'New Zealand Dollar',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   PEN: {
     symbol: 'S/',
     name: 'Peruvian Nuevo Sol',
-    minorUnits: 2,
     lowerUnitName: 'Céntimo',
     conversionRate: 100
   },
   PGK: {
     symbol: 'PGK',
     name: 'Papua New Guinean Kina',
-    minorUnits: 2,
     lowerUnitName: 'Toea',
     conversionRate: 100
   },
   PHP: {
     symbol: '₱',
     name: 'Philippine Peso',
-    minorUnits: 2,
     lowerUnitName: 'Centavo',
     conversionRate: 100
   },
   PKR: {
     symbol: '₨',
     name: 'Pakistani Rupee',
-    minorUnits: 2,
     lowerUnitName: 'Paisa',
     conversionRate: 100
   },
   QAR: {
     symbol: 'QR',
     name: 'Qatari Riyal',
-    minorUnits: 2,
     lowerUnitName: 'Dirham',
     conversionRate: 100
   },
   RUB: {
     symbol: '₽',
     name: 'Russian Ruble',
-    minorUnits: 2,
     lowerUnitName: 'Kopeck',
     conversionRate: 100
   },
   SAR: {
     symbol: 'SR',
     name: 'Saudi Riyal',
-    minorUnits: 2,
     lowerUnitName: 'Halala',
     conversionRate: 100
   },
   SCR: {
     symbol: 'SRe',
     name: 'Seychellois Rupee',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   SEK: {
     symbol: 'SEK',
     name: 'Swedish Krona',
-    minorUnits: 2,
     lowerUnitName: 'Öre',
     conversionRate: 100
   },
   SGD: {
     symbol: 'S$',
     name: 'Singapore Dollar',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
@@ -523,112 +482,96 @@ export const CURRENCIES = {
   SOS: {
     symbol: 'Sh.so.',
     name: 'Somali Shilling',
-    minorUnits: 2,
     lowerUnitName: 'Senti',
     conversionRate: 100
   },
   SSP: {
     symbol: 'SS£',
     name: 'South Sudanese Pound',
-    minorUnits: 2,
     lowerUnitName: 'Piaster',
     conversionRate: 100
   },
   SVC: {
     symbol: '₡',
     name: 'Salvadoran Colón',
-    minorUnits: 2,
     lowerUnitName: 'Centavo',
     conversionRate: 100
   },
   SZL: {
     symbol: 'E',
     name: 'Swazi Lilangeni',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   THB: {
     symbol: '฿',
     name: 'Thai Baht',
-    minorUnits: 2,
     lowerUnitName: 'Satang',
     conversionRate: 100
   },
   TTD: {
     symbol: 'TT$',
     name: 'Trinidad and Tobago Dollar',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   TZS: {
     symbol: 'Sh',
     name: 'Tanzanian Shilling',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   USD: {
     symbol: '$',
     name: 'United States Dollar',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   UYU: {
     symbol: '$U',
     name: 'Uruguayan Peso',
-    minorUnits: 2,
     lowerUnitName: 'Centésimo',
     conversionRate: 100
   },
   UZS: {
     symbol: "so'm",
     name: 'Uzbekistani Som',
-    minorUnits: 2,
     lowerUnitName: 'Tiyin',
     conversionRate: 100
   },
   YER: {
     symbol: '﷼',
     name: 'Yemeni Rial',
-    minorUnits: 2,
     lowerUnitName: 'Fils',
     conversionRate: 100
   },
   ZAR: {
     symbol: 'R',
     name: 'South African Rand',
-    minorUnits: 2,
     lowerUnitName: 'Cent',
     conversionRate: 100
   },
   KWD: {
     symbol: 'د.ك',
     name: 'Kuwaiti Dinar',
-    minorUnits: 3,
     lowerUnitName: 'Fils',
     conversionRate: 1000
   },
   BHD: {
     symbol: 'د.ب.',
     name: 'Bahraini Dinar',
-    minorUnits: 3,
     lowerUnitName: 'Fils',
     conversionRate: 1000
   },
   OMR: {
     symbol: 'ر.ع.',
     name: 'Omani Rial',
-    minorUnits: 3,
     lowerUnitName: 'Baisa',
     conversionRate: 1000
   },
   JPY: {
     symbol: '¥',
     name: 'Japanese Yen',
-    minorUnits: 0,
     lowerUnitName: '',
     conversionRate: 1
   }
