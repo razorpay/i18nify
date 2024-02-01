fb4af2d9073b79bc0004f1a6ca0f70f3ca3f6d52
avinashdvv <dvv.avinash@gmail.com>
2024-02-02 01:36:27 +0530

    feat: export the data mappers for phone number modules

--- packages/i18nify-js/src/modules/phoneNumber/index.ts
+++ packages/i18nify-js/src/modules/phoneNumber/index.ts
@@ -1,3 +1,9 @@
+// Exports the phone number utils
 export { default as isValidPhoneNumber } from './isValidPhoneNumber';
 export { default as formatPhoneNumber } from './formatPhoneNumber';
 export { default as parsePhoneNumber } from './parsePhoneNumber';
+
+// Exports the data mappers for utils
+export { PHONE_FORMATTER_MAPPER as COUNTRY_CODE_PHONE_FORMATTER_MAP } from './data/phoneFormatterMapper';
+export { DIAL_CODE_MAPPER as COUNTRY_CODE_DIAL_CODE_MAP } from './data/dialCodeMapper';
+export { PHONE_REGEX_MAPPER as COUNTRY_CODE_PHONE_REGEX_MAP } from './data/phoneRegexMapper';
