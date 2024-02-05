a8f5f6319fc744a8b441f66ff92ba3642e248c72
RgnDunes <divyansh.singh@razorpay.com>
2024-02-06 00:38:39 +0530

    modify getCountryFlagAsSvg

--- packages/i18nify-js/src/modules/geo/getCountryFlagAsSvg.ts
+++ packages/i18nify-js/src/modules/geo/getCountryFlagAsSvg.ts
@@ -1,11 +1,20 @@
 import { withErrorBoundary } from '../../common/errorBoundary';
 
-const getCountryFlagAsSvg = async (countryCode: string) => {
+/**
+ * Retrieves the SVG string for a given country code using dynamic import.
+ * SVGs are loaded on demand.
+ *
+ * @param countryCode The country code of the flag to retrieve.
+ * @returns A promise that resolves to the SVG string for the given country code.
+ */
+const getCountryFlagAsSvg = async (countryCode: string): Promise<string> => {
+  const code = countryCode.toUpperCase();
+
   try {
-    const countrySvg = await import(`./data/${countryCode}.svg`);
-    return countrySvg;
-  } catch (err) {
-    throw new Error('Invalid country code!');
+    const svgModule = await import(`./data/${code}.svg`);
+    return svgModule.default;
+  } catch (error) {
+    throw new Error(`Error loading the SVG for ${countryCode}: ${error}`);
   }
 };
 
