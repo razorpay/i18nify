2f13e3e33c462f9c37f6b8c3cca72d971ea8838b
RgnDunes <divyansh.singh@razorpay.com>
2024-02-07 19:40:36 +0530

    migrate console

--- packages/i18nify-js/src/index.ts
+++ packages/i18nify-js/src/index.ts
@@ -2,10 +2,6 @@ import loadPolyfills from './modules/.internal/polyfills/index';
 
 // Call the loadPolyfills function
 loadPolyfills()
-  .then(() => {
-    // Polyfills loaded successfully
-    console.info('i18nify polyfills loaded successfully !');
-  })
   .catch((error: any) => {
     // Handle any errors that occurred while loading polyfills
     console.error('Error loading i18nify polyfills:', error);
