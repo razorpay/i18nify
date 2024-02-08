bcb138cf3a0c7781d34c509d7919ec212d33b4cc
RgnDunes <divyansh.singh@razorpay.com>
2024-02-08 14:36:53 +0530

    add dynamic import in getDefaultLocaleForAllCountries

--- packages/i18nify-js/src/modules/core/getDefaultLocaleForAllCountries.ts
+++ packages/i18nify-js/src/modules/core/getDefaultLocaleForAllCountries.ts
@@ -1,12 +1,18 @@
 import { withErrorBoundary } from '../../common/errorBoundary';
-import { COUNTRY_TO_DEFAULT_LOCALES } from "./data/countryToDefaultLocales";
 
 /**
  * Retrieves a map of all countries to their default locale.
  * @returns A map where the key is the country code and the value is the default locale string.
  */
-const getDefaultLocaleForAllCountries = (): typeof COUNTRY_TO_DEFAULT_LOCALES => {
-    return COUNTRY_TO_DEFAULT_LOCALES;
+const getDefaultLocaleForAllCountries = async (): Promise<
+  typeof COUNTRY_TO_DEFAULT_LOCALES
+> => {
+  const { COUNTRY_TO_DEFAULT_LOCALES } = await import(
+    './data/countryToDefaultLocales'
+  );
+  return COUNTRY_TO_DEFAULT_LOCALES;
 };
 
-export default withErrorBoundary<typeof getDefaultLocaleForAllCountries>(getDefaultLocaleForAllCountries);
+export default withErrorBoundary<typeof getDefaultLocaleForAllCountries>(
+  getDefaultLocaleForAllCountries,
+);
