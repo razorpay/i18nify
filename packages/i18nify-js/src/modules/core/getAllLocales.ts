0cab1b63b7c7aa54a4275e4fd91c62ee9fe56dbc
RgnDunes <divyansh.singh@razorpay.com>
2024-02-08 14:34:26 +0530

    add dynamic import in getAllLocales

--- packages/i18nify-js/src/modules/core/getAllLocales.ts
+++ packages/i18nify-js/src/modules/core/getAllLocales.ts
@@ -1,11 +1,13 @@
 import { withErrorBoundary } from '../../common/errorBoundary';
-import { COUNTRY_TO_ALL_LOCALES } from './data/countryToAllLocales';
 
 /**
  * Retrieves a map of all countries to their respective locales.
  * @returns A map where the key is the country code and the value is an array of locale strings.
  */
-const getAllLocales = (): typeof COUNTRY_TO_ALL_LOCALES => {
+const getAllLocales = async (): Promise<typeof COUNTRY_TO_ALL_LOCALES> => {
+    const { COUNTRY_TO_ALL_LOCALES } = await import(
+        './data/countryToAllLocales'
+      );
     return COUNTRY_TO_ALL_LOCALES;
 };
 
