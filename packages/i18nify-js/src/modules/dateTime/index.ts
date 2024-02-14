2f49bdcbc1facb166438e8a610221dbca831bdda
RgnDunes <divyansh.singh@razorpay.com>
2024-02-14 15:50:09 +0530

    [fix]: remove additional exports from date module

--- packages/i18nify-js/src/modules/dateTime/index.ts
+++ packages/i18nify-js/src/modules/dateTime/index.ts
@@ -3,18 +3,10 @@
  * in a locale-sensitive manner using the JavaScript Intl API & Date object.
  */
 
-export { default as add } from './add';
 export { default as formatDate } from './formatDate';
 export { default as formatDateTime } from './formatDateTime';
 export { default as formatTime } from './formatTime';
-export { default as getQuarter } from './getQuarter';
 export { default as getRelativeTime } from './getRelativeTime';
-export { default as getWeek } from './getWeek';
 export { default as getWeekdays } from './getWeekdays';
-export { default as isAfter } from './isAfter';
-export { default as isBefore } from './isBefore';
-export { default as isLeapYear } from './isLeapYear';
-export { default as isSameDay } from './isSameDay';
 export { default as isValidDate } from './isValidDate';
 export { default as parseDateTime } from './parseDateTime';
-export { default as subtract } from './subtract';
