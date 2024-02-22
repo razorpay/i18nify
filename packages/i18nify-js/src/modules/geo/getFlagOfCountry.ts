090093366f102aed0ee31074c791ae539c1fc6b7
RgnDunes <divyansh.singh@razorpay.com>
2024-02-22 18:24:04 +0530

    add getFlagOfCountry

---
+++ packages/i18nify-js/src/modules/geo/getFlagOfCountry.ts
@@ -0,0 +1,23 @@
+import { withErrorBoundary } from '../../common/errorBoundary';
+import { LIST_OF_ALL_COUNTRIES } from './data/listOfAllCountries';
+
+/**
+ * Retrieves the URL for the flag of a given country.
+ *
+ * This function checks if the provided country code is valid by verifying it against
+ * a predefined list of country codes. If the country code is valid, it constructs and
+ * returns a URL pointing to the flag image for that country. If the country code is not
+ * found in the list, it throws an error indicating that the country code is invalid.
+ *
+ * @param countryCode - The country code for which to retrieve the flag URL. Must be a valid code from the list of all countries.
+ * @returns The URL of the flag image for the given country code.
+ * @throws {Error} If the country code is not in the list of valid country codes.
+ */
+const getFlagOfCountry = (countryCode: string): string => {
+  if (!LIST_OF_ALL_COUNTRIES.includes(countryCode)) {
+    throw new Error(`Invalid country code: ${countryCode}`);
+  }
+  return `https://betacdn.np.razorpay.in/static/assets/flags/${countryCode.toLowerCase()}.svg`;
+};
+
+export default withErrorBoundary<typeof getFlagOfCountry>(getFlagOfCountry);
