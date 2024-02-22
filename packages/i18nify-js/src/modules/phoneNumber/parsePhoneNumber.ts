d06eb54a8144573269d10a8d5f7a4db260c8ff2d
RgnDunes <divyansh.singh@razorpay.com>
2024-02-22 15:04:10 +0530

    modify detectCountryAndDialCodeFromPhone

--- packages/i18nify-js/src/modules/phoneNumber/parsePhoneNumber.ts
+++ packages/i18nify-js/src/modules/phoneNumber/parsePhoneNumber.ts
@@ -1,14 +1,14 @@
 import { withErrorBoundary } from '../../common/errorBoundary';
-import { DIAL_CODE_MAPPER } from './data/dialCodeMapper';
 import { PHONE_FORMATTER_MAPPER } from './data/phoneFormatterMapper';
 import formatPhoneNumber from './formatPhoneNumber';
-import { detectCountryCodeFromDialCode, cleanPhoneNumber } from './utils';
+import { detectCountryAndDialCodeFromPhone, cleanPhoneNumber } from './utils';
 
 interface PhoneInfo {
   countryCode: string;
   dialCode: string;
   formattedPhoneNumber: string;
   formatTemplate: string;
+  phoneNumber: string;
 }
 
 // Parses a given phone number, identifies its country code (if not provided), and returns an object with details including the country code, formatted phone number, dial code, and format template.
@@ -20,12 +20,14 @@ const parsePhoneNumber = (phoneNumber: string, country?: string): PhoneInfo => {
   phoneNumber = phoneNumber.toString();
   phoneNumber = cleanPhoneNumber(phoneNumber);
 
+  const countryData = detectCountryAndDialCodeFromPhone(phoneNumber);
   // Detect or validate the country code
   const countryCode =
     country && country in PHONE_FORMATTER_MAPPER
       ? country
-      : detectCountryCodeFromDialCode(phoneNumber);
+      : countryData.countryCode;
 
+  const dialCode = countryData.dialCode;
   // Format the phone number using the detected/validated country code
   const formattedPhoneNumber = formatPhoneNumber(phoneNumber, countryCode);
 
@@ -34,10 +36,11 @@ const parsePhoneNumber = (phoneNumber: string, country?: string): PhoneInfo => {
 
   if (!pattern)
     return {
-      countryCode: countryCode || '',
-      dialCode: '',
+      countryCode,
+      dialCode,
       formattedPhoneNumber: phoneNumber,
       formatTemplate: '',
+      phoneNumber,
     };
 
   // Count the number of 'x' characters in the format pattern
@@ -51,36 +54,16 @@ const parsePhoneNumber = (phoneNumber: string, country?: string): PhoneInfo => {
   // Calculate the difference between phoneNumber length and 'x' characters count in pattern
   const diff = phoneNumber.length - charCountInFormatterPattern;
 
-  // Extract the dialCode from the phoneNumber
-  let dialCode = phoneNumber.slice(0, diff);
-
   // Obtain the format template associated with the countryCode
   const formatTemplate = PHONE_FORMATTER_MAPPER[countryCode];
 
-  if (!dialCode && countryCode) {
-    // Iterate over each key in the DIAL_CODE_MAPPER object
-    for (const key in DIAL_CODE_MAPPER) {
-      // Check if the key is actually a property of 'DIAL_CODE_MAPPER' and not inherited from its prototype
-      if (DIAL_CODE_MAPPER.hasOwnProperty(key)) {
-        // Get the array of country codes associated with the current key
-        const countryCodes = DIAL_CODE_MAPPER[key];
-        // Check if the provided countryCode exists in the current array of country codes
-        if (countryCodes.includes(countryCode)) {
-          // If found, append `+` and store the dialCode
-          // The key is originally a string because object keys are always strings in JavaScript
-          dialCode = `+${key}`;
-          break;
-        }
-      }
-    }
-  }
-
   // Return the parsed phone number information
   return {
+    phoneNumber: phoneNumber.slice(diff),
     countryCode,
-    formattedPhoneNumber,
     dialCode,
-    formatTemplate,
+    formattedPhoneNumber: pattern ? formattedPhoneNumber : phoneNumber,
+    formatTemplate: formatTemplate || '',
   };
 };
 
