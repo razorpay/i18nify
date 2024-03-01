2c4c5d2493740d45d9345f3e129dc347c3f03b25
RgnDunes <divyansh.singh@razorpay.com>
2024-03-01 15:00:52 +0530

    add desc in phone number dial code mapper

--- packages/i18nify-js/src/modules/phoneNumber/data/dialCodeMapper.ts
+++ packages/i18nify-js/src/modules/phoneNumber/data/dialCodeMapper.ts
@@ -4,227 +4,227 @@ import { CountryCodeType } from '../../types/geo';
 
 export const DIAL_CODE_MAPPER: { [key: number]: CountryCodeType[] } = {
   1: [
-    'US',
-    'AG',
-    'AI',
-    'AS',
-    'BB',
-    'BM',
-    'BS',
-    'CA',
-    'DM',
-    'DO',
-    'GD',
-    'GU',
-    'JM',
-    'KN',
-    'KY',
-    'LC',
-    'MP',
-    'MS',
-    'PR',
-    'TC',
-    'TT',
-    'VC',
-    'VG',
-    'VI',
+    'US', // United States
+    'AG', // Antigua and Barbuda
+    'AI', // Anguilla
+    'AS', // American Samoa
+    'BB', // Barbados
+    'BM', // Bermuda
+    'BS', // Bahamas
+    'CA', // Canada
+    'DM', // Dominica
+    'DO', // Dominican Republic
+    'GD', // Grenada
+    'GU', // Guam
+    'JM', // Jamaica
+    'KN', // Saint Kitts and Nevis
+    'KY', // Cayman Islands
+    'LC', // Saint Lucia
+    'MP', // Northern Mariana Islands
+    'MS', // Montserrat
+    'PR', // Puerto Rico
+    'TC', // Turks and Caicos Islands
+    'TT', // Trinidad and Tobago
+    'VC', // Saint Vincent and the Grenadines
+    'VG', // British Virgin Islands
+    'VI', // U.S. Virgin Islands
   ],
-  7: ['RU', 'KZ'],
-  20: ['EG'],
-  27: ['ZA'],
-  30: ['GR'],
-  31: ['NL'],
-  32: ['BE'],
-  33: ['FR'],
-  34: ['ES'],
-  36: ['HU'],
-  39: ['IT', 'VA'],
-  40: ['RO'],
-  41: ['CH'],
-  43: ['AT'],
-  44: ['GB'],
-  45: ['DK'],
-  46: ['SE'],
-  47: ['NO'],
-  48: ['PL'],
-  49: ['DE'],
-  51: ['PE'],
-  52: ['MX'],
-  53: ['CU'],
-  54: ['AR'],
-  55: ['BR'],
-  56: ['CL'],
-  57: ['CO'],
-  58: ['VE'],
-  60: ['MY'],
-  61: ['AU'],
-  62: ['ID'],
-  63: ['PH'],
-  64: ['NZ'],
-  65: ['SG'],
-  66: ['TH'],
-  81: ['JP'],
-  82: ['KR'],
-  84: ['VN'],
-  86: ['CN'],
-  90: ['TR'],
-  91: ['IN'],
-  92: ['PK'],
-  93: ['AF'],
-  94: ['LK'],
-  95: ['MM'],
-  98: ['IR'],
-  211: ['SS'],
-  212: ['MA'],
-  213: ['DZ'],
-  216: ['TN'],
-  218: ['LY'],
-  220: ['GM'],
-  221: ['SN'],
-  222: ['MR'],
-  223: ['ML'],
-  224: ['GN'],
-  225: ['CI'],
-  226: ['BF'],
-  227: ['NE'],
-  228: ['TG'],
-  229: ['BJ'],
-  230: ['MU'],
-  231: ['LR'],
-  232: ['SL'],
-  233: ['GH'],
-  234: ['NG'],
-  235: ['TD'],
-  236: ['CF'],
-  237: ['CM'],
-  238: ['CV'],
-  239: ['ST'],
-  240: ['GQ'],
-  241: ['GA'],
-  242: ['CG'],
-  243: ['CD'],
-  244: ['AO'],
-  245: ['GW'],
-  248: ['SC'],
-  249: ['SD'],
-  250: ['RW'],
-  251: ['ET'],
-  252: ['SO'],
-  253: ['DJ'],
-  254: ['KE'],
-  255: ['TZ'],
-  256: ['UG'],
-  257: ['BI'],
-  258: ['MZ'],
-  260: ['ZM'],
-  261: ['MG'],
-  262: ['RE', 'YT'],
-  263: ['ZW'],
-  264: ['NA'],
-  265: ['MW'],
-  266: ['LS'],
-  267: ['BW'],
-  268: ['SZ'],
-  269: ['KM'],
-  290: ['SH'],
-  291: ['ER'],
-  297: ['AW'],
-  298: ['FO'],
-  299: ['GL'],
-  350: ['GI'],
-  351: ['PT'],
-  352: ['LU'],
-  353: ['IE'],
-  354: ['IS'],
-  355: ['AL'],
-  356: ['MT'],
-  357: ['CY'],
-  358: ['FI'],
-  359: ['BG'],
-  370: ['LT'],
-  371: ['LV'],
-  372: ['EE'],
-  373: ['MD'],
-  374: ['AM'],
-  375: ['BY'],
-  376: ['AD'],
-  377: ['MC'],
-  378: ['SM'],
-  380: ['UA'],
-  381: ['RS'],
-  382: ['ME'],
-  383: ['XK'],
-  385: ['HR'],
-  386: ['SI'],
-  387: ['BA'],
-  389: ['MK'],
-  420: ['CZ'],
-  421: ['SK'],
-  423: ['LI'],
-  500: ['FK'],
-  501: ['BZ'],
-  502: ['GT'],
-  503: ['SV'],
-  504: ['HN'],
-  505: ['NI'],
-  506: ['CR'],
-  507: ['PA'],
-  509: ['HT'],
-  590: ['MF', 'FW'],
-  591: ['BO'],
-  592: ['GY'],
-  593: ['EC'],
-  594: ['GF'],
-  595: ['PY'],
-  597: ['SR'],
-  598: ['UY'],
-  599: ['CW', 'BQ'],
-  670: ['TL'],
-  673: ['BN'],
-  674: ['NR'],
-  675: ['PG'],
-  676: ['TO'],
-  677: ['SB'],
-  678: ['VU'],
-  679: ['FJ'],
-  680: ['PW'],
-  682: ['CK'],
-  683: ['NU'],
-  685: ['WS'],
-  686: ['KI'],
-  687: ['NC'],
-  688: ['TV'],
-  689: ['PF'],
-  691: ['FM'],
-  692: ['MH'],
-  850: ['KP'],
-  852: ['HK'],
-  853: ['MO'],
-  855: ['KH'],
-  856: ['LA'],
-  880: ['BD'],
-  886: ['TW'],
-  960: ['MV'],
-  961: ['LB'],
-  962: ['JO'],
-  963: ['SY'],
-  964: ['IQ'],
-  965: ['KW'],
-  966: ['SA'],
-  967: ['YE'],
-  968: ['OM'],
-  970: ['PS'],
-  971: ['AE'],
-  972: ['IL'],
-  973: ['BH'],
-  974: ['QA'],
-  975: ['BT'],
-  976: ['MN'],
-  977: ['NP'],
-  992: ['TJ'],
-  993: ['TM'],
-  994: ['AZ'],
-  995: ['GE'],
-  996: ['KG'],
-  998: ['UZ'],
+  7: ['RU', 'KZ'], // Russian Federation, Kazakhstan
+  20: ['EG'], // Egypt
+  27: ['ZA'], // South Africa
+  30: ['GR'], // Greece
+  31: ['NL'], // Netherlands
+  32: ['BE'], // Belgium
+  33: ['FR'], // France
+  34: ['ES'], // Spain
+  36: ['HU'], // Hungary
+  39: ['IT', 'VA'], // Italy, Vatican City
+  40: ['RO'], // Romania
+  41: ['CH'], // Switzerland
+  43: ['AT'], // Austria
+  44: ['GB'], // United Kingdom
+  45: ['DK'], // Denmark
+  46: ['SE'], // Sweden
+  47: ['NO'], // Norway
+  48: ['PL'], // Poland
+  49: ['DE'], // Germany
+  51: ['PE'], // Peru
+  52: ['MX'], // Mexico
+  53: ['CU'], // Cuba
+  54: ['AR'], // Argentina
+  55: ['BR'], // Brazil
+  56: ['CL'], // Chile
+  57: ['CO'], // Colombia
+  58: ['VE'], // Venezuela
+  60: ['MY'], // Malaysia
+  61: ['AU'], // Australia
+  62: ['ID'], // Indonesia
+  63: ['PH'], // Philippines
+  64: ['NZ'], // New Zealand
+  65: ['SG'], // Singapore
+  66: ['TH'], // Thailand
+  81: ['JP'], // Japan
+  82: ['KR'], // South Korea
+  84: ['VN'], // Vietnam
+  86: ['CN'], // China
+  90: ['TR'], // Turkey
+  91: ['IN'], // India
+  92: ['PK'], // Pakistan
+  93: ['AF'], // Afghanistan
+  94: ['LK'], // Sri Lanka
+  95: ['MM'], // Myanmar
+  98: ['IR'], // Iran
+  211: ['SS'], // South Sudan
+  212: ['MA'], // Morocco
+  213: ['DZ'], // Algeria
+  216: ['TN'], // Tunisia
+  218: ['LY'], // Libya
+  220: ['GM'], // Gambia
+  221: ['SN'], // Senegal
+  222: ['MR'], // Mauritania
+  223: ['ML'], // Mali
+  224: ['GN'], // Guinea
+  225: ['CI'], // Côte d'Ivoire
+  226: ['BF'], // Burkina Faso
+  227: ['NE'], // Niger
+  228: ['TG'], // Togo
+  229: ['BJ'], // Benin
+  230: ['MU'], // Mauritius
+  231: ['LR'], // Liberia
+  232: ['SL'], // Sierra Leone
+  233: ['GH'], // Ghana
+  234: ['NG'], // Nigeria
+  235: ['TD'], // Chad
+  236: ['CF'], // Central African Republic
+  237: ['CM'], // Cameroon
+  238: ['CV'], // Cape Verde
+  239: ['ST'], // Sao Tome and Principe
+  240: ['GQ'], // Equatorial Guinea
+  241: ['GA'], // Gabon
+  242: ['CG'], // Congo (Brazzaville)
+  243: ['CD'], // Congo (Kinshasa)
+  244: ['AO'], // Angola
+  245: ['GW'], // Guinea-Bissau
+  248: ['SC'], // Seychelles
+  249: ['SD'], // Sudan
+  250: ['RW'], // Rwanda
+  251: ['ET'], // Ethiopia
+  252: ['SO'], // Somalia
+  253: ['DJ'], // Djibouti
+  254: ['KE'], // Kenya
+  255: ['TZ'], // Tanzania
+  256: ['UG'], // Uganda
+  257: ['BI'], // Burundi
+  258: ['MZ'], // Mozambique
+  260: ['ZM'], // Zambia
+  261: ['MG'], // Madagascar
+  262: ['RE', 'YT'], // Réunion, Mayotte
+  263: ['ZW'], // Zimbabwe
+  264: ['NA'], // Namibia
+  265: ['MW'], // Malawi
+  266: ['LS'], // Lesotho
+  267: ['BW'], // Botswana
+  268: ['SZ'], // Eswatini
+  269: ['KM'], // Comoros
+  290: ['SH'], // Saint Helena
+  291: ['ER'], // Eritrea
+  297: ['AW'], // Aruba
+  298: ['FO'], // Faroe Islands
+  299: ['GL'], // Greenland
+  350: ['GI'], // Gibraltar
+  351: ['PT'], // Portugal
+  352: ['LU'], // Luxembourg
+  353: ['IE'], // Ireland
+  354: ['IS'], // Iceland
+  355: ['AL'], // Albania
+  356: ['MT'], // Malta
+  357: ['CY'], // Cyprus
+  358: ['FI'], // Finland
+  359: ['BG'], // Bulgaria
+  370: ['LT'], // Lithuania
+  371: ['LV'], // Latvia
+  372: ['EE'], // Estonia
+  373: ['MD'], // Moldova
+  374: ['AM'], // Armenia
+  375: ['BY'], // Belarus
+  376: ['AD'], // Andorra
+  377: ['MC'], // Monaco
+  378: ['SM'], // San Marino
+  380: ['UA'], // Ukraine
+  381: ['RS'], // Serbia
+  382: ['ME'], // Montenegro
+  383: ['XK'], // Kosovo
+  385: ['HR'], // Croatia
+  386: ['SI'], // Slovenia
+  387: ['BA'], // Bosnia and Herzegovina
+  389: ['MK'], // North Macedonia
+  420: ['CZ'], // Czech Republic
+  421: ['SK'], // Slovakia
+  423: ['LI'], // Liechtenstein
+  500: ['FK'], // Falkland Islands
+  501: ['BZ'], // Belize
+  502: ['GT'], // Guatemala
+  503: ['SV'], // El Salvador
+  504: ['HN'], // Honduras
+  505: ['NI'], // Nicaragua
+  506: ['CR'], // Costa Rica
+  507: ['PA'], // Panama
+  509: ['HT'], // Haiti
+  590: ['MF', 'FW'], // Saint Martin, French West Indies
+  591: ['BO'], // Bolivia
+  592: ['GY'], // Guyana
+  593: ['EC'], // Ecuador
+  594: ['GF'], // French Guiana
+  595: ['PY'], // Paraguay
+  597: ['SR'], // Suriname
+  598: ['UY'], // Uruguay
+  599: ['CW', 'BQ'], // Curaçao, Caribbean Netherlands
+  670: ['TL'], // Timor-Leste
+  673: ['BN'], // Brunei
+  674: ['NR'], // Nauru
+  675: ['PG'], // Papua New Guinea
+  676: ['TO'], // Tonga
+  677: ['SB'], // Solomon Islands
+  678: ['VU'], // Vanuatu
+  679: ['FJ'], // Fiji
+  680: ['PW'], // Palau
+  682: ['CK'], // Cook Islands
+  683: ['NU'], // Niue
+  685: ['WS'], // Samoa
+  686: ['KI'], // Kiribati
+  687: ['NC'], // New Caledonia
+  688: ['TV'], // Tuvalu
+  689: ['PF'], // French Polynesia
+  691: ['FM'], // Micronesia
+  692: ['MH'], // Marshall Islands
+  850: ['KP'], // North Korea
+  852: ['HK'], // Hong Kong
+  853: ['MO'], // Macao
+  855: ['KH'], // Cambodia
+  856: ['LA'], // Laos
+  880: ['BD'], // Bangladesh
+  886: ['TW'], // Taiwan
+  960: ['MV'], // Maldives
+  961: ['LB'], // Lebanon
+  962: ['JO'], // Jordan
+  963: ['SY'], // Syria
+  964: ['IQ'], // Iraq
+  965: ['KW'], // Kuwait
+  966: ['SA'], // Saudi Arabia
+  967: ['YE'], // Yemen
+  968: ['OM'], // Oman
+  970: ['PS'], // Palestine
+  971: ['AE'], // United Arab Emirates
+  972: ['IL'], // Israel
+  973: ['BH'], // Bahrain
+  974: ['QA'], // Qatar
+  975: ['BT'], // Bhutan
+  976: ['MN'], // Mongolia
+  977: ['NP'], // Nepal
+  992: ['TJ'], // Tajikistan
+  993: ['TM'], // Turkmenistan
+  994: ['AZ'], // Azerbaijan
+  995: ['GE'], // Georgia
+  996: ['KG'], // Kyrgyzstan
+  998: ['UZ'], // Uzbekistan
 };
