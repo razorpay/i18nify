13cebdc105b4efe5c1272e7881622503273c7e2d
RgnDunes <divyansh.singh@razorpay.com>
2024-03-07 02:11:55 +0530

    re-export classes from @internationalized/date

--- packages/i18nify-js/src/modules/dateTime/index.ts
+++ packages/i18nify-js/src/modules/dateTime/index.ts
@@ -1,5 +1,3 @@
-import {Calendar, CalendarDate, CalendarDateTime, Time, ZonedDateTime} from '@internationalized/date';
-
 /**
  * This module provides functions for formatting and manipulating dates and times
  * in a locale-sensitive manner using the JavaScript Intl API & Date object.
@@ -12,3 +10,6 @@ export { default as getRelativeTime } from './getRelativeTime';
 export { default as getWeekdays } from './getWeekdays';
 export { default as isValidDate } from './isValidDate';
 export { default as parseDateTime } from './parseDateTime';
+
+// Re-exporting Calendar, CalendarDate, CalendarDateTime, Time, ZonedDateTime directly
+export { Calendar, CalendarDate, CalendarDateTime, Time, ZonedDateTime } from '@internationalized/date';
