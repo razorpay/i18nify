8c706313015f044e37750c28d6e986b87b4ce518
Debanshu Rout <debanshu.rout@razorpay.com>
2024-03-22 12:57:04 +0530

    add readme

--- packages/i18nify-go/example/example.go
+++ packages/i18nify-go/example/example.go
@@ -11,33 +11,32 @@ func main() {
 	countryIN := i18nify_go.NewCountry("IN")
 
 	metaDataIN := countryIN.GetCountryMetadata()
-	fmt.Println(metaDataIN)
-	fmt.Println(metaDataIN.CountryName)
-	fmt.Println(metaDataIN.Currency)
-	fmt.Println(metaDataIN.DialCode)
-	fmt.Println(metaDataIN.Timezones)
-	fmt.Println(metaDataIN.DefaultLocale)
+	fmt.Println(metaDataIN.CountryName)   //India
+	fmt.Println(metaDataIN.Currency)      //[INR]
+	fmt.Println(metaDataIN.DialCode)      //+91
+	fmt.Println(metaDataIN.Timezones)     //Asia/Kolkata:{UTC +05:30}
+	fmt.Println(metaDataIN.DefaultLocale) //en_IN
 
 	//INR
 	currencyIN := countryIN.GetCountryCurrency()
-	fmt.Println(currencyIN[0].Name)
-	fmt.Println(currencyIN[0].Symbol)
+	fmt.Println(currencyIN[0].Name)   //Indian Rupee
+	fmt.Println(currencyIN[0].Symbol) //₹
 
 	//India PhoneNumber
 	phoneNumberIN := countryIN.GetCountryPhoneNumber()
-	fmt.Println(phoneNumberIN.DialCode)
-	fmt.Println(phoneNumberIN.Regex)
+	fmt.Println(phoneNumberIN.DialCode) //+91
+	fmt.Println(phoneNumberIN.Regex)    // /^(?:(?:\+|0{0,2})91\s*[-]?\s*|[0]?)?[6789]\d{9}$/
 
 	//India States
 	subdivisions := countryIN.GetCountrySubDivisions()
-	fmt.Println(subdivisions.GetCountryName())
+	fmt.Println(subdivisions.GetCountryName()) //India
 
 	state := subdivisions.GetStates()["KA"]
-	fmt.Println(state.GetName())
-	fmt.Println(state.GetCities()[0])
+	fmt.Println(state.GetName())      //Karnataka
+	fmt.Println(state.GetCities()[0]) //{Yellāpur nan Asia/Kolkata [581337 581337 ...}
 
 	//USD
 	currencyUS := currency.GetCurrencyInformation("USD")
-	fmt.Println(currencyUS.Name)
-	fmt.Println(currencyUS.Symbol)
+	fmt.Println(currencyUS.Name)   //US Dollar
+	fmt.Println(currencyUS.Symbol) //$
 }
--- packages/i18nify-go/readme.md
+++ packages/i18nify-go/readme.md
@@ -1,8 +1,27 @@
-# i18nify-go
-_A one-stop solution built in golang to provide internationalization support._
+## i18nify-go
+_A comprehensive solution written in Go to facilitate internationalization._
 
-# Go SDK:
+## Go Software Development Kit (SDK)
 
+This package is compatible with modern versions of Go in module mode. If you have Go installed, you can add the package to your current development module and its dependencies by running:
+
+`go get github.com/razorpay/i18nify/packages/i18nify-go`
+
+will resolve and add the package to the current development module, along with its dependencies.
+
+Alternatively, you can achieve the same result by importing the package in your code:
+
+`import "github.com/razorpay/i18nify/packages/i18nify-go"`
+
+Then, run go get without any parameters.
+
+To use the latest version of this repository's main branch, execute the following command:
+
+`go get github.com/razorpay/i18nify/packages/i18nify-go@master`
+
+
+
+## Quicktype(beta)
 Convert json to json schema
 1. quicktype data/<attribute>_v1.json -l schema -o data/schema/<attribute>_schema.json
 
