0a99b1e3e5df8f4bda6cac4cf933ad5ac2375606
RgnDunes <divyansh.singh@razorpay.com>
2024-03-31 01:11:37 +0530

    [chore]: migrate getCurrencySymbol, getCurrencyList from modular data to geo data

--- packages/i18nify-js/src/modules/currency/getCurrencyList.ts
+++ packages/i18nify-js/src/modules/currency/getCurrencyList.ts
@@ -1,8 +1,8 @@
-import { CURRENCIES } from './data/currencies';
 import { withErrorBoundary } from '../../common/errorBoundary';
+import CURRENCY_INFO from '../../../../../i18nify-data/currency/data.json';
 
 const getCurrencyList = () => {
-  return CURRENCIES;
+  return CURRENCY_INFO.currency_information;
 };
 
 export default withErrorBoundary<typeof getCurrencyList>(getCurrencyList);
--- packages/i18nify-js/src/modules/currency/getCurrencySymbol.ts
+++ packages/i18nify-js/src/modules/currency/getCurrencySymbol.ts
@@ -1,9 +1,11 @@
-import { CURRENCIES } from './data/currencies';
 import { withErrorBoundary } from '../../common/errorBoundary';
 import { CurrencyCodeType } from './types';
+import CURRENCY_INFO from '../../../../../i18nify-data/currency/data.json';
 
 const getCurrencySymbol = (currencyCode: CurrencyCodeType): string => {
-  if (currencyCode in CURRENCIES) return CURRENCIES[currencyCode]?.symbol;
+  const currencyInformation = CURRENCY_INFO.currency_information;
+  if (currencyCode in currencyInformation)
+    return currencyInformation[currencyCode]?.symbol;
   else throw new Error('Invalid currencyCode!');
 };
 
