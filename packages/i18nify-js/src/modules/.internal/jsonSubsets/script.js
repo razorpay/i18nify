4fd76778daef26d7b08fc4723e6109e92729a30e
RgnDunes <divyansh.singh@razorpay.com>
2024-04-03 02:02:21 +0530

    [fix]: update script.js

--- packages/i18nify-js/src/modules/.internal/jsonSubsets/script.js
+++ packages/i18nify-js/src/modules/.internal/jsonSubsets/script.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const currencyOriginalData = require('../../../../../../i18nify-data/currency/data.json');
 
 const currencyGeoDataFilePath =
