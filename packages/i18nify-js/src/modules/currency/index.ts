dff98c0d8c4e0e547e6faa029679671da0aa8c22
RgnDunes <divyansh.singh@razorpay.com>
2024-04-02 14:54:00 +0530

    [fix]: remove unused code

--- packages/i18nify-js/src/modules/currency/index.ts
+++ packages/i18nify-js/src/modules/currency/index.ts
@@ -4,6 +4,4 @@ export { default as getCurrencySymbol } from './getCurrencySymbol';
 export { default as formatNumberByParts } from './formatNumberByParts';
 export { default as convertToMajorUnit } from './convertToMajorUnit';
 export { default as convertToMinorUnit } from './convertToMinorUnit';
-export { default as getPhysicalCurrencyDenominations } from './getPhysicalCurrencyDenominations';
-export { default as getCurrencyName } from './getCurrencyName';
 export type { CurrencyCodeType } from './types';
