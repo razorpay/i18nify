92cfe2601ad7821085d40567048585171e28f12d
RgnDunes <divyansh.singh@razorpay.com>
2024-04-03 01:43:26 +0530

    [fix]: fix build issues

--- packages/i18nify-js/src/modules/currency/__tests__/convertToMinorUnit.test.ts
+++ packages/i18nify-js/src/modules/currency/__tests__/convertToMinorUnit.test.ts
@@ -12,7 +12,7 @@ describe('currency - convertToMinorUnit', () => {
   ];
 
   testCases.forEach(({ amount, currency, expectedResult }) => {
-    it(`should correctly convert ${amount} of minor unit ${currency} to ${expectedResult}`, () => {
+    it(`should correctly convert ${amount} of minor unit ${String(currency)} to ${expectedResult}`, () => {
       const result = convertToMinorUnit(amount, { currency: currency });
       expect(result).toBe(expectedResult);
     });
@@ -21,7 +21,6 @@ describe('currency - convertToMinorUnit', () => {
   it('should throw an error for unsupported currency codes', () => {
     const unsupportedCurrencyCode = 'XXX';
     expect(() => {
-      // @ts-expect-error intented invalid currencyCode for testing
       convertToMinorUnit(100, { currency: unsupportedCurrencyCode });
     }).toThrow('Unsupported currency XXX');
   });
