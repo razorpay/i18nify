550f6e446d93a8f7d63ac7e73294bdc90cd2852e
RgnDunes <divyansh.singh@razorpay.com>
2024-04-04 03:32:38 +0530

    [resolve]: resolve review comments

--- packages/i18nify-js/src/modules/.internal/jsonSubsets/script.ts
+++ packages/i18nify-js/src/modules/.internal/jsonSubsets/script.ts
@@ -1,59 +1,19 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import currencyOriginalData from '#/i18nify-data/currency/data.json';
+import { createModuleSubsetFile } from './jsonSubsetGenerator';
+import {
+  currencyGeoDataFilePath,
+  transformCurrencyData,
+} from './transformerFunctions';
+import { CurrencyInformation, CurrencySubsetData } from './types';
 
-const currencyGeoDataFilePath =
-  './src/modules/.internal/jsonSubsets/currency/currencyDataSubset.json';
+const subsetMap = [
+  [currencyOriginalData, currencyGeoDataFilePath, transformCurrencyData],
+];
 
-interface CurrencyDetail {
-  name: string;
-  numeric_code: string;
-  minor_unit: string;
-  symbol: string;
-  physical_currency_denominations: string[];
-}
-
-interface CurrencyInformation {
-  [key: string]: CurrencyDetail;
-}
-
-interface CurrencySubsetData {
-  [key: string]: {
-    name: string;
-    minor_unit: string;
-    symbol: string;
-  };
-}
-
-const currencySubsetData: CurrencySubsetData = Object.entries(
-  (currencyOriginalData as CurrencyInformation).currency_information,
-).reduce((acc: CurrencySubsetData, [key, value]: [string, CurrencyDetail]) => {
-  acc[key] = {
-    name: value.name,
-    minor_unit: value.minor_unit,
-    symbol: value.symbol,
-  };
-  return acc;
-}, {});
-
-// Check if the file exists before trying to delete it, then use async functions to handle file operations
-const createCurrencyGeoDataFile = async () => {
-  if (fs.existsSync(currencyGeoDataFilePath)) {
-    // If the file exists, delete it
-    fs.unlinkSync(currencyGeoDataFilePath);
-  }
-
-  const dirPath = path.dirname(currencyGeoDataFilePath);
-  // Create directory recursively if it doesn't exist
-  fs.mkdirSync(dirPath, { recursive: true });
-
-  // Write the file asynchronously
-  await fs.promises.writeFile(
-    currencyGeoDataFilePath,
-    JSON.stringify(currencySubsetData, null, 2),
-    'utf8',
+subsetMap.forEach((module) => {
+  createModuleSubsetFile<CurrencyInformation, CurrencySubsetData>(
+    module[0],
+    module[1],
+    module[2],
   );
-  console.log('Currency module json data subset created successfully.');
-};
-
-createCurrencyGeoDataFile();
+});
---
+++ packages/i18nify-js/src/modules/.internal/jsonSubsets/transformerFunctions.ts
@@ -0,0 +1,28 @@
+import {
+  CurrencyInformation,
+  CurrencySubsetData,
+  TransformFunction,
+} from './types';
+
+export const currencyGeoDataFilePath =
+  './src/modules/.internal/jsonSubsets/currency/currencyDataSubset.json';
+
+// Currency-specific transformation logic
+export const transformCurrencyData: TransformFunction<
+  CurrencyInformation,
+  CurrencySubsetData
+> = (currencyOriginalData) => {
+  return Object.entries(currencyOriginalData.currency_information).reduce(
+    (acc, [key, value]) => {
+      acc[key] = {
+        name: value.name,
+        minor_unit: value.minor_unit,
+        symbol: value.symbol,
+      };
+      return acc;
+    },
+    {},
+  );
+};
+
+// Other module-specific transformation logic here
