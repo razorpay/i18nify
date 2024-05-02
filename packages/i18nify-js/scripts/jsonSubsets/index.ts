d5adae741f8e37bb30e170da7a4b54c63e0b5124
RgnDunes <divyansh.singh@razorpay.com>
2024-05-02 11:23:40 +0530

    [test]: dummy commit

--- packages/i18nify-js/scripts/jsonSubsets/index.ts
+++ packages/i18nify-js/scripts/jsonSubsets/index.ts
@@ -1,5 +1,6 @@
 import transformFormatterConfig from './phoneNumber/transformFormatterConfig';
 import transformRegexConfig from './phoneNumber/transformRegexConfig';
+
 import createModuleSubsetFile from './createFile';
 
 createModuleSubsetFile(transformFormatterConfig());
