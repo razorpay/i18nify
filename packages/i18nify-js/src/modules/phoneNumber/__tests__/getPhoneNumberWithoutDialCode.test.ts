af70fe1348f2aab7e92930e777f16f2761e1cf89
RgnDunes <divyansh.singh@razorpay.com>
2024-06-20 18:09:28 +0530

    [test]: add missing UTs

---
+++ packages/i18nify-js/src/modules/phoneNumber/__tests__/getPhoneNumberWithoutDialCode.test.ts
@@ -0,0 +1,28 @@
+import { getPhoneNumberWithoutDialCode } from '../utils';
+
+describe('getPhoneNumberWithoutDialCode', () => {
+  test('should correctly remove the dial code from a phone number', () => {
+    const phoneNumber = '+1-800-123-4567';
+    const result = getPhoneNumberWithoutDialCode(phoneNumber);
+   
+    expect(result).toBe('8001234567');
+  });
+
+  test('should return the phone number unchanged if no dial code is present', () => {
+    const phoneNumber = '7394 926646';
+    const result = getPhoneNumberWithoutDialCode(phoneNumber);
+    expect(result).toBe('7394926646');
+  });
+
+  test('should handle international phone numbers', () => {
+    const phoneNumber = '+44 7911 123456';
+    const result = getPhoneNumberWithoutDialCode(phoneNumber);
+    expect(result).toBe('7911123456');
+  });
+
+  test('should process numbers with unusual dial codes', () => {
+    const phoneNumber = '+91-9876543210';
+    const result = getPhoneNumberWithoutDialCode(phoneNumber);
+    expect(result).toBe('9876543210'); 
+  });
+});
