243859a6fcb23f2510472c43a8baba3bffa3ecea
Debanshu Rout <debanshu.rout@razorpay.com>
2025-01-29 15:01:08 +0530

    fix: update IN,MY country data [ROW-541] (#198)
    * update IN,SG country data
    
    * refactor fe change to accomodate new country data structure
    
    * revert i18nify source data url to master
    
    * Create forty-falcons-run.md
    
    * add missing UT
    
    * fix e2e
    
    * modify playwright config
    
    * replace installation with docker image
    
    * update docker image
    
    * switch to non-root user
    
    * explicit installation of msedge
    
    * explicit installation of msedge
    
    * dummy
    
    * dummy
    
    * dummy
    
    * revert yaml changes
    
    ---------
    
    Co-authored-by: Divyansh Singh <divyansh.singh@razorpay.com>

--- packages/i18nify-js/src/modules/geo/constants.ts
+++ packages/i18nify-js/src/modules/geo/constants.ts
@@ -3,6 +3,6 @@ export const FLAG_4X3_BASE_PATH =
 export const FLAG_BASE_PATH = 'https://flagcdn.com';
 
 export const I18NIFY_DATA_SOURCE =
-  'https://raw.githubusercontent.com/razorpay/i18nify/master/i18nify-data';
+  'https://raw.githubusercontent.com/razorpay/i18nify/update_country_data/i18nify-data';
 
 export const I18NIFY_DATA_SUPPORTED_COUNTRIES = ['IN', 'MY', 'SG', 'US'];
