47a4b49703364a97d5b6052d6843a2724c58bf6a
RgnDunes <divyansh.singh@razorpay.com>
2025-03-24 15:14:46 +0530

    feat: add versioned data source support for i18nify-js
    Introduce version mapping and versioned data source functionality to ensure compatibility between i18nify-js and i18nify-data. This includes fetching version mappings from a remote JSON file and dynamically constructing data source paths based on the current package version.

--- packages/i18nify-js/src/modules/shared/sourceConstants.ts
+++ packages/i18nify-js/src/modules/shared/sourceConstants.ts
@@ -2,7 +2,21 @@ export const FLAG_4X3_BASE_PATH =
   'https://unpkg.com/@razorpay/i18nify-js/lib/assets/flags';
 export const FLAG_BASE_PATH = 'https://flagcdn.com';
 
+import { getVersionedDataPath, VersionMapping } from './versionConfig';
+
 export const I18NIFY_DATA_SOURCE =
-  'https://raw.githubusercontent.com/razorpay/i18nify/master/i18nify-data';
+  'https://raw.githubusercontent.com/razorpay/i18nify/master';
+
+export async function getVersionedDataSource(
+  dataType: keyof VersionMapping,
+): Promise<string> {
+  try {
+    const versionedPath = await getVersionedDataPath(dataType);
+    return `${I18NIFY_DATA_SOURCE}${versionedPath.startsWith('/') ? versionedPath : `/${versionedPath}`}`;
+  } catch (error) {
+    console.error('Error getting versioned data source:', error);
+    throw error;
+  }
+}
 
 export const I18NIFY_DATA_SUPPORTED_COUNTRIES = ['IN', 'MY', 'SG', 'US'];
