0d98d1a721f46da04a4238b48cfe58242ad1a8a2
rzp-Piyush <piyush.tiwari@razropay.com>
2025-12-10 18:11:22 +0530

    data loader template change

--- i18nify-data/go/phone-number/data_loader.go
+++ i18nify-data/go/phone-number/data_loader.go
@@ -4,7 +4,6 @@ package phonenumber
 import (
 	"embed"
 	"encoding/json"
-	"sync"
 )
 
 //go:embed data/data.json
@@ -15,29 +14,27 @@ type root struct {
 	CountryTeleInformation map[string]*CountryTeleInformation `json:"country_tele_information"`
 }
 
-var (
-	dataOnce sync.Once
-	data     map[string]*CountryTeleInformation
-)
+var data map[string]*CountryTeleInformation
+
+func init() {
+	// Read data from embedded filesystem
+	jsonBytes, err := dataFS.ReadFile("data/data.json")
+	if err != nil {
+		panic("failed to read embedded phone-number data: " + err.Error())
+	}
+	
+	var r root
+	if err := json.Unmarshal(jsonBytes, &r); err != nil {
+		panic("failed to parse embedded phone-number data: " + err.Error())
+	}
+	data = r.CountryTeleInformation
+	if data == nil {
+		data = map[string]*CountryTeleInformation{}
+	}
+}
 
 // GetData returns a map keyed by code to CountryTeleInformation.
 func GetData() map[string]*CountryTeleInformation {
-	dataOnce.Do(func() {
-		// Read data from embedded filesystem
-		jsonBytes, err := dataFS.ReadFile("data/data.json")
-		if err != nil {
-			panic("failed to read embedded phone-number data: " + err.Error())
-		}
-		
-		var r root
-		if err := json.Unmarshal(jsonBytes, &r); err != nil {
-			panic("failed to parse embedded phone-number data: " + err.Error())
-		}
-		data = r.CountryTeleInformation
-		if data == nil {
-			data = map[string]*CountryTeleInformation{}
-		}
-	})
 	return data
 }
 
