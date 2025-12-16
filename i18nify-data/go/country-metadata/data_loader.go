88a8825a36c93333eef3aa169041484f956e45ed
GitHub Action <action@github.com>
2025-12-16 09:16:07 +0000

    chore: auto-regenerate country-metadata Go package

--- i18nify-data/go/country-metadata/data_loader.go
+++ i18nify-data/go/country-metadata/data_loader.go
@@ -17,20 +17,20 @@ type root struct {
 var data map[string]*MetadataInformation
 
 func init() {
-	// Read data from embedded filesystem
-	jsonBytes, err := dataFS.ReadFile("data/data.json")
-	if err != nil {
-		panic("failed to read embedded country metadata data: " + err.Error())
-	}
-	
-	var r root
-	if err := json.Unmarshal(jsonBytes, &r); err != nil {
-		panic("failed to parse embedded country metadata data: " + err.Error())
-	}
-	data = r.MetadataInformation
-	if data == nil {
-		data = map[string]*MetadataInformation{}
-	}
+		// Read data from embedded filesystem
+		jsonBytes, err := dataFS.ReadFile("data/data.json")
+		if err != nil {
+			panic("failed to read embedded country metadata data: " + err.Error())
+		}
+		
+		var r root
+		if err := json.Unmarshal(jsonBytes, &r); err != nil {
+			panic("failed to parse embedded country metadata data: " + err.Error())
+		}
+		data = r.MetadataInformation
+		if data == nil {
+			data = map[string]*MetadataInformation{}
+		}
 }
 
 // GetData returns a map keyed by code to MetadataInformation.
