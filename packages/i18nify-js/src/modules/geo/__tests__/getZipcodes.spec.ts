a8bbcfea68436d3f8183c263346dc2b9a65dde82
upendra-kushwaha <upendra.kushwaha@razorpay.com>
2026-06-12 18:21:28 +0530

    fix(ci): stabilize geo blackbox tests

--- packages/i18nify-js/src/modules/geo/__tests__/getZipcodes.spec.ts
+++ packages/i18nify-js/src/modules/geo/__tests__/getZipcodes.spec.ts
@@ -12,9 +12,9 @@ test.describe('getZipcodes', () => {
   }) => {
     await injectScript(
       page,
-      `await getZipcodes('IN', 'TN').then(res => res[0])`,
+      `await getZipcodes('IN', 'DL').then(res => String(res.includes('110092')))`,
     );
 
-    await assertScriptText(page, '124508');
+    await assertScriptText(page, 'true');
   });
 });
