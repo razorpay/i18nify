916a3a0eea726aa7a975d86c428be9ed47e3a06d
upendra-kushwaha <upendra.kushwaha@razorpay.com>
2026-06-12 16:29:01 +0530

    test(geo): stabilize browser checks

--- packages/i18nify-js/src/modules/geo/__tests__/getCities.spec.ts
+++ packages/i18nify-js/src/modules/geo/__tests__/getCities.spec.ts
@@ -1,9 +1,18 @@
 import { test } from '@playwright/test';
 import getCities from '../getCities';
 import { assertScriptText, injectScript } from '../../../blackbox/utils';
+import { CountryDetailType } from '../types';
+import { INDIA_DATA } from '../mocks/country';
 
 test.describe('getCities', () => {
   test.beforeEach(async ({ page }) => {
+    global.fetch = () =>
+      Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => Promise.resolve<CountryDetailType>(INDIA_DATA),
+      } as Response);
+
     await page.exposeFunction('getCities', getCities);
   });
 
