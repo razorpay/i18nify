851c732520f01121a94746d7a77cc673b6e014f4
upendra-kushwaha <upendra.kushwaha@razorpay.com>
2026-06-13 18:50:52 +0530

    feat(postalCode): merge PR 704 into geo branch

--- packages/i18nify-js/src/modules/geo/__tests__/getZipcodes.spec.ts
+++ packages/i18nify-js/src/modules/geo/__tests__/getZipcodes.spec.ts
@@ -1,10 +1,21 @@
 import { test } from '@playwright/test';
 import getZipcodes from '../getZipcodes';
 import { assertScriptText, injectScript } from '../../../blackbox/utils';
+import { INDIA_DATA } from '../mocks/country';
+
+const originalFetch = global.fetch;
 
 test.describe('getZipcodes', () => {
   test.beforeEach(async ({ page }) => {
     await page.exposeFunction('getZipcodes', getZipcodes);
+    global.fetch = async () =>
+      ({
+        json: async () => INDIA_DATA,
+      }) as Response;
+  });
+
+  test.afterEach(() => {
+    global.fetch = originalFetch;
   });
 
   test('should print the correct zipcodes for provided country and state code', async ({
@@ -12,9 +23,9 @@ test.describe('getZipcodes', () => {
   }) => {
     await injectScript(
       page,
-      `await getZipcodes('IN', 'TN').then(res => res.includes('639103'))`,
+      `await getZipcodes('IN', 'NL').then(res => res[0])`,
     );
 
-    await assertScriptText(page, 'true');
+    await assertScriptText(page, '124508');
   });
 });
