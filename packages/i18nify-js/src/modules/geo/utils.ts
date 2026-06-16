1e5a7a7d738093782ee9e859504dae674f2f8c42
upendra-kushwaha <upendra.kushwaha@razorpay.com>
2026-06-16 16:57:23 +0530

    chore(banking): remove geo folder from banking PR

--- packages/i18nify-js/src/modules/geo/utils.ts
+++
@@ -1,7 +0,0 @@
-import { LIST_OF_ALL_COUNTRIES } from './data/listOfAllCountries';
-
-export function isCountryValid(_countryCode: string): boolean {
-  const countryCode = _countryCode.toUpperCase();
-  // @ts-expect-error countryCode here can be a random string
-  return LIST_OF_ALL_COUNTRIES.includes(countryCode);
-}
