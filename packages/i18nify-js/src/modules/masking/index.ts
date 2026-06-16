75c5188c02195f9198f65f8612e49a412243278f
upendra-kushwaha <upendra.kushwaha@razorpay.com>
2026-06-16 18:24:10 +0530

    chore(masking): delete masking module — functions moved to banking and phoneNumber

--- packages/i18nify-js/src/modules/masking/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as maskCardNumber } from './maskCardNumber';
-export { default as maskPhoneNumber } from './maskPhoneNumber';
-export type { MaskCardOptions, MaskPhoneOptions } from './types';
-export {
-  DEFAULT_MASK_CHAR,
-  DEFAULT_VISIBLE_DIGITS,
-  DEFAULT_CARD_GROUP_SIZE,
-  DEFAULT_CARD_GROUP_SEPARATOR,
-} from './constants';
