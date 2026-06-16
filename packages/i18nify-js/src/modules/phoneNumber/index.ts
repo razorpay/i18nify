d3a53338604385a0a4383e4cfa42bb1d34427f1f
upendra-kushwaha <upendra.kushwaha@razorpay.com>
2026-06-16 19:14:16 +0530

    feat(phoneNumber): add maskPhoneNumber to JS and Go modules

--- packages/i18nify-js/src/modules/phoneNumber/index.ts
+++ packages/i18nify-js/src/modules/phoneNumber/index.ts
@@ -4,5 +4,6 @@ export { default as parsePhoneNumber } from './parsePhoneNumber';
 export { default as getDialCodes } from './getDialCodes';
 export { default as getDialCodeByCountryCode } from './getDialCodeByCountryCode';
 export { default as getMaskedPhoneNumber } from './getMaskedPhoneNumber';
-export type { GetMaskedPhoneNumberOptions } from './types';
+export { default as maskPhoneNumber } from './maskPhoneNumber';
+export type { GetMaskedPhoneNumberOptions, MaskPhoneOptions } from './types';
 export { MaskingStyle } from './constants';
