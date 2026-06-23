7f58bd306185cc6ffbd97d6e1e3e18eb379c6362
upendra-kushwaha <upendra.kushwaha@razorpay.com>
2026-06-23 15:39:16 +0530

    fix(geo): import address data via #/i18nify-data alias, remove local copy

--- packages/i18nify-js/src/modules/geo/getAddressInfo.ts
+++ packages/i18nify-js/src/modules/geo/getAddressInfo.ts
@@ -1,5 +1,5 @@
 import type { AddressCodeType, AddressType } from './types';
-import rawData from './data/addressData.json';
+import rawData from '#/i18nify-data/address/data.json';
 
 const addressData = (rawData as unknown as { address_format_information: Record<string, AddressType> })
   .address_format_information;
