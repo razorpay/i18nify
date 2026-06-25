150603d4d01b16c996bc07f43b60e05aec14f2b3
upendra-kushwaha <upendra.kushwaha@razorpay.com>
2026-06-25 15:10:33 +0530

    chore: revert getCities.spec.ts and getZipcodes.spec.ts to master state

--- packages/i18nify-js/src/modules/geo/__tests__/getZipcodes.spec.ts
+++ packages/i18nify-js/src/modules/geo/__tests__/getZipcodes.spec.ts
@@ -12,9 +12,9 @@ test.describe('getZipcodes', () => {
   }) => {
     await injectScript(
       page,
-      `await getZipcodes('IN', 'TN').then(res => String(Array.isArray(res) && res.length > 0 && typeof res[0] === 'string'))`,
+      `await getZipcodes('IN', 'TN').then(res => res[0])`,
     );
 
-    await assertScriptText(page, 'true');
+    await assertScriptText(page, '124508');
   });
 });
