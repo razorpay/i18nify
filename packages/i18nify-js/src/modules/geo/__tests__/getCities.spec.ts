2f7a5cf0ff96275a4dd3b9849617e8ccbeeeacc7
upendra-kushwaha <upendra.kushwaha@razorpay.com>
2026-06-25 17:26:06 +0530

    revert: restore getCities.spec.ts and getZipcodes.spec.ts to master state

--- packages/i18nify-js/src/modules/geo/__tests__/getCities.spec.ts
+++ packages/i18nify-js/src/modules/geo/__tests__/getCities.spec.ts
@@ -1,21 +1,10 @@
 import { test } from '@playwright/test';
 import getCities from '../getCities';
 import { assertScriptText, injectScript } from '../../../blackbox/utils';
-import { INDIA_DATA } from '../mocks/country';
-
-const originalFetch = global.fetch;
 
 test.describe('getCities', () => {
   test.beforeEach(async ({ page }) => {
-    global.fetch = async () =>
-      ({
-        json: async () => INDIA_DATA,
-      }) as Response;
     await page.exposeFunction('getCities', getCities);
-  });
-
-  test.afterEach(() => {
-    global.fetch = originalFetch;
   });
 
   test('should print the correct city name for provided country and state code', async ({
--- packages/i18nify-js/src/modules/geo/__tests__/getZipcodes.spec.ts
+++ packages/i18nify-js/src/modules/geo/__tests__/getZipcodes.spec.ts
@@ -1,21 +1,10 @@
 import { test } from '@playwright/test';
 import getZipcodes from '../getZipcodes';
 import { assertScriptText, injectScript } from '../../../blackbox/utils';
-import { INDIA_DATA } from '../mocks/country';
-
-const originalFetch = global.fetch;
 
 test.describe('getZipcodes', () => {
   test.beforeEach(async ({ page }) => {
-    global.fetch = async () =>
-      ({
-        json: async () => INDIA_DATA,
-      }) as Response;
     await page.exposeFunction('getZipcodes', getZipcodes);
-  });
-
-  test.afterEach(() => {
-    global.fetch = originalFetch;
   });
 
   test('should print the correct zipcodes for provided country and state code', async ({
@@ -23,9 +12,9 @@ test.describe('getZipcodes', () => {
   }) => {
     await injectScript(
       page,
-      `await getZipcodes('IN', 'DL').then(res => res[0])`,
+      `await getZipcodes('IN', 'TN').then(res => res[0])`,
     );
 
-    await assertScriptText(page, '110092');
+    await assertScriptText(page, '124508');
   });
 });
